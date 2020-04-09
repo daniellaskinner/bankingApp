d1e114b415336ac461eb95d943b3bd1bb9413af6
daniellaskinner <daniellaskinner120@gmail.com>
2020-04-09 10:26:16 +0100

    installed body parser and mongodb node modules and required into project

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+//set up the app
 const express = require('express');
 const app = express();
 const port = 1000;
 app.get('/', (req,res) => {res.send('Hello welcome to the banking app')});
 
-
-
+//require in extra node packages
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 
 
