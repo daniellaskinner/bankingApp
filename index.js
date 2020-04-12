9a480bb6101865ec39739223079fa454fa2cd7e2
daniellaskinner <daniellaskinner120@gmail.com>
2020-04-12 17:06:13 +0100

    updated add new account route to use async and await

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const jsonParser = bodyParser.json();
 //display all accounts route
 app.get('/accounts', (req, res) => {
     MongoClient.connect(url, {useUnifiedTopology: true}, async (err, client) => {
-        console.log('connected to account database successfully');
         let bankDb = client.db('bankingApp');
         let accounts = await displayAllAccounts(bankDb);
         res.json({"accounts" : accounts});
@@ -33,63 +32,77 @@ let displayAllAccounts = async (db) => {
 
 //add account route
     app.post('/accounts', jsonParser, (req, res) => {
-        //new object to go into db this will be via frontend later on
         const newAccount = {
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             accountNumber: req.body.accountNumber,
             accountBalance: req.body.accountBalance,
         };
+        let status = 500;
+        let response = {
+            "success": false,
+            "message": "Account could not be added."
+        };
 
-        MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+        MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, async (err, client) => {
             let bankDb = client.db('bankingApp');
-            addNewAccount(bankDb, newAccount, (documents) => {
-                if(documents.insertedCount === 1){
-                    res.send('Successfully added new account for: ' + newAccount.firstName + " " + newAccount.lastName);
-                } else {
-                    res.send('Unable to add new account for this client.');
-                }
-                client.close();
-            });
+            let accountAddition = await addNewAccount(bankDb, newAccount);
+            if (accountAddition.insertedCount === 1) {
+                response.success = true;
+                status = 200;
+                response.message = 'Successfully added new account for: ' + newAccount.firstName + " " + newAccount.lastName;
+                res.status(status).send(response);
+            } else {
+                response.success = false;
+                status = 500;
+                response.message = 'Unable to add new account for this client.';
+                res.status(status).send(response);
+            }
+            await client.close();
         });
+
     });
 
 //function add account
-    var addNewAccount = (db, newAccountToSend, callback) => {
+    let addNewAccount = async (db, newAccountToSend) => {
         let collection = db.collection('accounts');
         //insert json object into the collection
-        collection.insertOne(newAccountToSend, (err, documents) => {
-            callback(documents);
-        });
+        let result = await collection.insertOne(newAccountToSend);
+        return result;
     };
 
 
-//edit account route (for updating balance/adding funds)
-    app.put('/accounts', jsonParser, (req, res) => {
-        const dataToSend = {
-            id: ObjectId(req.body.id),
-            depositToAdd: req.body.deposit
-        };
-        MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, async (err, client) => {
-            let bankDb = client.db('bankingApp');
+// JSON TO TEST BELOW
+// {"id":"5e8eed83270adc042416361b", "deposit": "135"}
 
-            let documents = await updateAccountBalance(bankDb, dataToSend)
-                if (documents.modifiedCount === 1) {
-                    res.send('Successfully updated account balance');
-                } else {
-                    res.send('Unable to process your deposit.');
-                }
-                client.close();
-            });
-    });
 
-// function update account balance
-let updateAccountBalance = async(db, deposit) => {
-    let collection = db.collection('accounts');
-    //increment the account balance by the deposit
-    let result = await collection.updateOne({_id : deposit.id}, {$inc: {accountBalance: deposit.depositToAdd}});
-       return result;
-};
+
+// //edit account route (for updating balance/adding funds)
+//     app.put('/accounts', jsonParser, (req, res) => {
+//         const dataToSend = {
+//             id: ObjectId(req.body.id),
+//             depositToAdd: req.body.deposit
+//         };
+//         MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, async (err, client) => {
+//             let bankDb = client.db('bankingApp');
+//
+//             let documents = await updateAccountBalance(bankDb, dataToSend)
+//                 if (documents.modifiedCount === 1) {
+//                     res.send('Successfully updated account balance');
+//                 } else {
+//                     res.send('Unable to process your deposit.');
+//                 }
+//                 client.close();
+//             });
+//     });
+//
+// // function update account balance
+// let updateAccountBalance = async(db, deposit) => {
+//     let collection = db.collection('accounts');
+//     //increment the account balance by the deposit
+//     let result = await collection.updateOne({_id : deposit.id}, {$inc: {accountBalance: deposit.depositToAdd}});
+//        return result;
+// };
 
 
 //listener
